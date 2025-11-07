a45fe2afeb4708504c871fa83c2afdcd62438f31
speckerf <felixspecker@hotmail.ch>
2025-11-07 14:39:12 +0100

    add entry point script (run_export.py) when run as script (not clip), and necessary requirements.txt to reproduce the environment

---
+++ run_export.py
@@ -0,0 +1,88 @@
+# run_export.py
+from __future__ import annotations
+
+from pathlib import Path
+
+import ee
+import typer
+import yaml
+from loguru import logger
+
+from gee_biophys.config import ConfigParams
+from gee_biophys.s2_export import export_image
+from gee_biophys.s2_input import load_s2_input
+from gee_biophys.s2_predict import biophys_predict
+from gee_biophys.utils import (
+    get_system_index,
+    initialize_export_location,
+    update_image_metadata,
+)
+
+
+def load_params(path: str) -> ConfigParams:
+    with open(path) as f:
+        data = yaml.safe_load(f)
+    return ConfigParams(**data)
+
+
+def validate_config(path: Path):
+    if not path or not path.exists():
+        raise typer.BadParameter(
+            "Please provide a valid path to a YAML configuration file, e.g.:\n"
+            "  gee-biophys --config example_configs/minimal_example.yaml"
+        )
+    return path
+
+
+app = typer.Typer(
+    help="Custom time-series export of LAIe/FAPAR/FCOVER with YAML params."
+)
+
+
+@app.command(help="Run the export using the provided YAML configuration.")
+def run(
+    config: Path = typer.Option(
+        ...,
+        "--config",
+        "-c",
+        help="Path to params YAML file.",
+        callback=validate_config,
+        readable=True,
+    )
+):
+    # <---- Setup ---->
+    cfg = load_params(str(config))
+
+    # default initialization / or use designated service account
+    ee.Initialize(project=cfg.export.project_id)
+
+    initialize_export_location(cfg)
+
+    # <---- Main Loop ---->
+    for interval_start, interval_end in cfg.temporal.iter_date_ranges():
+
+        # <---- Load Input ---->
+        logger.info(
+            f"Processing interval: {interval_start.strftime('%Y-%m-%d')} to {interval_end.strftime('%Y-%m-%d')}"
+        )
+        imgc = load_s2_input(cfg, interval_start, interval_end)
+
+        # <---- Prediction ---->
+        output_image = biophys_predict(cfg, imgc)
+
+        # <---- Update metadata ---->
+        output_image = update_image_metadata(
+            output_image, interval_start, interval_end, cfg
+        )
+        filename = get_system_index(cfg, interval_start, interval_end)
+
+        # <---- Export  ---->
+        export_image(output_image, filename, cfg)
+
+    logger.info(
+        f"Done! Exports have been started. Please check task status to see if they completed successfully."
+    )
+
+
+if __name__ == "__main__":
+    app()
